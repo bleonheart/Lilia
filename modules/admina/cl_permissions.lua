a57c57db81a0035bd6911ac21035ba539733f1d8
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-13 08:24:07 +0100

    hehe

--- modules/admina/cl_permissions.lua
+++ modules/admina/cl_permissions.lua
@@ -1,11 +1,7 @@
 local MODULE = MODULE
 lia.admin = lia.admin or {}
 lia.admin.permissions = lia.admin.permissions or {}
-
-netstream.Hook("nutscript_updateAdminPermissions", function(info)
-	lia.admin.permissions = info
-end)
-
+netstream.Hook("nutscript_updateAdminPermissions", function(info) lia.admin.permissions = info end)
 function MODULE:InitPostEntity()
 	netstream.Start("nutscript_requestAdminPermissions")
 end
--- modules/admina/module.lua
+++ modules/admina/module.lua
@@ -3,27 +3,21 @@ MODULE.name = "Admin"
 MODULE.author = "rusty"
 MODULE.desc = "Stop using paid admin mods, idiots."
 MODULE.language = "english"
-
 lia.admin = lia.admin or {}
-lia.admin.commands = lia.admin.commands or {noclip = true}
+lia.admin.commands = lia.admin.commands or {
+	noclip = true
+}
 
 lia.util.include("sh_permissions.lua")
 lia.util.include("cl_permissions.lua")
 lia.util.include("cl_plugin.lua")
-
 lia.util.include("sh_commands.lua")
-
-if SERVER then
-	lia.util.include("sv_permissions.lua")
-	lia.util.include("sv_bans.lua")
-end
-
-
-
+lia.util.include("sv_permissions.lua")
+lia.util.include("sv_bans.lua")
 function MODULE:PlayerNoClip(client, state)
-	if (client:hasPermission("noclip")) then
+	if client:hasPermission("noclip") then
 		if SERVER then
-			if (state) then
+			if state then
 				client.nutObsData = {client:GetPos(), client:EyeAngles()}
 				client:SetNoDraw(true)
 				client:SetNotSolid(true)
@@ -33,10 +27,9 @@ function MODULE:PlayerNoClip(client, state)
 				client:SetNoTarget(true)
 				hook.Run("OnPlayerObserve", client, state)
 			else
-				if (client.nutObsData) then
-					if (client:GetInfoNum("nut_obstpback", 0) > 0) then
+				if client.nutObsData then
+					if client:GetInfoNum("nut_obstpback", 0) > 0 then
 						local position, angles = client.nutObsData[1], client.nutObsData[2]
-
 						timer.Simple(0, function()
 							client:SetPos(position)
 							client:SetEyeAngles(angles)
@@ -56,51 +49,43 @@ function MODULE:PlayerNoClip(client, state)
 				hook.Run("OnPlayerObserve", client, state)
 			end
 		end
-		
 		return true
 	end
 end
 
 function MODULE:InitializedModules()
-	for cmd,info in next, lia.command.list do
+	for cmd, info in next, lia.command.list do
 		if info.group or info.superAdminOnly or info.adminOnly then
-			info.onCheckAccess = function(client)
-				return client:hasPermission(cmd)
-			end
+			info.onCheckAccess = function(client) return client:hasPermission(cmd) end
 			info.onRun = function(client, arguments)
-				if !info.onCheckAccess(client) then
+				if not info.onCheckAccess(client) then
 					return "@noPerm"
 				else
 					return info._onRun(client, arguments)
 				end
 			end
-			
+
 			lia.admin.commands[cmd] = true
 		end
 	end
-	
+
 	lia.command.findPlayer = function(client, name)
 		local calling_func = debug.getinfo(2)
 		local command
 		local target = type(name) == "string" and lia.util.findPlayer(name) or NULL
-
-		for cmd,info in next, lia.command.list do
-			if info._onRun == calling_func.func then
-				command = info
-			end
+		for cmd, info in next, lia.command.list do
+			if info._onRun == calling_func.func then command = info end
 		end
 
-		if (IsValid(target)) then
+		if IsValid(target) then
 			if command and (command.adminOnly or command.superAdminOnly) then
 				local target_group = lia.admin.permissions[target:GetUserGroup()]
 				local client_group = lia.admin.permissions[client:GetUserGroup()]
-				
 				if target_group.position < client_group.position then
 					client:notifyLocalized("plyCantTarget")
 					return
 				end
 			end
-		
 			return target
 		else
 			client:notifyLocalized("plyNoExist")
@@ -108,46 +93,44 @@ function MODULE:InitializedModules()
 	end
 end
 
-
-concommand.Add("plysetgroup", function( ply, cmd, args )
-    if !IsValid(ply) then
+concommand.Add("plysetgroup", function(ply, cmd, args)
+	if not IsValid(ply) then
 		local target = lia.util.findPlayer(args[1])
 		if IsValid(target) then
 			if lia.admin.permissions[args[2]] then
 				lia.admin.setPlayerGroup(target, args[2])
 			else
-				MsgC(Color(200,20,20), "[NutScript Admin] Error: usergroup not found.\n")
+				MsgC(Color(200, 20, 20), "[NutScript Admin] Error: usergroup not found.\n")
 			end
 		else
-			MsgC(Color(200,20,20), "[NutScript Admin] Error: specified player not found.\n")
+			MsgC(Color(200, 20, 20), "[NutScript Admin] Error: specified player not found.\n")
 		end
 	end
 end)
 
-
-concommand.Add("nsadmin_createownergroup", function( ply, cmd, args )
-    if !IsValid(ply) then
+concommand.Add("nsadmin_createownergroup", function(ply, cmd, args)
+	if not IsValid(ply) then
 		lia.admin.createGroup("owner", {
 			position = 0,
 			admin = false,
 			superadmin = true,
 			permissions = {},
 		})
-		
-		for cmd,_ in next, lia.admin.commands do
+
+		for cmd, _ in next, lia.admin.commands do
 			lia.admin.permissions["owner"].permissions[cmd] = true
 		end
-		
+
 		lia.admin.save(true)
 	end
 end)
 
-concommand.Add("nsadmin_wipegroups", function( ply, cmd, args )
-    if !IsValid(ply) then
-		for k,v in next, player.GetAll() do
+concommand.Add("nsadmin_wipegroups", function(ply, cmd, args)
+	if not IsValid(ply) then
+		for k, v in next, player.GetAll() do
 			v:SetUserGroup("user")
 		end
-	
+
 		lia.admin.permissions = {}
 		lia.admin.save(true)
 	end
