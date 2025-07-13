dea248bd20808a62f15503e384d74fb058f18191
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-13 21:20:04 +0100

    xd

--- modules/oil/entities/entities/lia_oilpump/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.PrintName = "Oil Pump"
-ENT.Category = "Lilia"
-ENT.Spawnable = true
-ENT.DrawEntityInfo = true
-
-function ENT:isReady()
-    return CurTime() >= self:getNetVar("ready", 0)
-end
