01a2cf602ce207e63f00832998f61f257a08f96e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-13 11:16:14 +0100

    Update ulx.lua

--- gamemode/core/libraries/compatibility/ulx.lua
+++ gamemode/core/libraries/compatibility/ulx.lua
@@ -1,1 +1,24 @@
-﻿lia.error("ULX isnt supported")
+﻿local psaString = [[
+/*------------------------------------------------------------
+
+PUBLIC SERVICE ANNOUNCEMENT FOR NUTSCRIPT SERVER OWNERS
+
+There is a ENOURMOUS performance issue with ULX Admin mod.
+Nutscript Development Team found ULX is the main issue
+that make the server freeze when player count is higher
+than 20-30. The duration of freeze will be increased as you get
+more players on your server.
+
+If you're planning to open big server with ULX/ULib, Nutscript
+Development Team does not recommend your plan. Server Performance
+Issues with ULX/Ulib on your server will be ignored and we're
+going to consider that you're taking the risk of ULX/Ulib's
+critical performance issue.
+
+Nutscript 1.2 only displays this message when you have ULX or
+ULib on your server.
+
+                               -Nutscript Development Team
+
+*/------------------------------------------------------------]]
+MsgC(Color(255, 0, 0), psaString .. "\n")
