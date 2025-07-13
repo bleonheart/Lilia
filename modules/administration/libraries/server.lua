c72e7e21fba1136193e598990170710b23f91560
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-13 16:43:16 +0100

    Update server.lua

--- modules/administration/libraries/server.lua
+++ modules/administration/libraries/server.lua
@@ -182,8 +182,10 @@ hook.Add("OnDatabaseLoaded", "lia.admin.bans.loadBanlist", function()
     end)
 end)
 
-lia.admin.returnPositions = lia.admin.returnPositions or {}
-hook.Add("PlayerSay", "liaAdminGag", function(client) if client:getNetVar("liaGagged") then return "" end end)
+function MODULE:PlayerSay(client)
+    if client:getNetVar("liaGagged") then return "" end
+end
+
 function MODULE:OnReloaded()
     for _, client in player.Iterator() do
         if IsValid(client) and client:IsPlayer() then client:ConCommand("spawnmenu_reload") end
