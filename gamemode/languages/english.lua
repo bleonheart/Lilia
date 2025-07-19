b79bf9c92448b6215de7d523107856aef958a4e5
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-19 22:23:06 +0100

    Add item inspect sharing

--- gamemode/languages/english.lua
+++ gamemode/languages/english.lua
@@ -186,6 +186,7 @@ LANGUAGE = {
     useTip = "Uses the item.",
     equipTip = "Equips the item.",
     rotateTip = "Rotates the item.",
+    inspectOfferTip = "Offers your item for inspection to the person you're looking at.",
     moneyLimit = "You've reached your money limit of %s%s. %s%s has been dropped.",
     playtimeYour = "Your playtime is %s hours, %s minutes, and %s seconds.",
     playtimeError = "Could not retrieve your playtime.",
