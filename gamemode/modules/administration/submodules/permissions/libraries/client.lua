6416aff02940f5abad68bce9ecbbaa1c01db7bff
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-20 10:59:51 +0100

    feat(anticheat): flag cheaters and add hook

--- gamemode/modules/administration/submodules/permissions/libraries/client.lua
+++ gamemode/modules/administration/submodules/permissions/libraries/client.lua
@@ -19,7 +19,12 @@ function MODULE:HUDPaint()
         if not IsValid(ent) or ent == client then continue end
         local entityType, label
         if ent:IsPlayer() and lia.option.get("espPlayers") then
-            entityType, label = "Players", ent:Name():gsub("#", "\226\128\139#")
+            entityType = "Players"
+            if ent:getNetVar("cheater") then
+                label = "CHEATER"
+            else
+                label = ent:Name():gsub("#", "\226\128\139#")
+            end
         elseif ent.isItem and ent:isItem() and lia.option.get("espItems") then
             entityType = "Items"
             local itemTable = ent.getItemTable and ent:getItemTable()
@@ -41,7 +46,9 @@ function MODULE:HUDPaint()
         local factor = 1 - math.Clamp(distanceSq / maxDistanceSq, 0, 1)
         local size = math.max(20, 48 * factor)
         local alpha = math.Clamp(255 * factor, 120, 255)
+        local cheater = ent:getNetVar("cheater", false)
         local colorToUse = ColorAlpha(lia.config.get("esp" .. entityType .. "Color") or Color(255, 255, 255), alpha)
+        if cheater then colorToUse = ColorAlpha(Color(255, 0, 0), alpha) end
         local x, y = math.Clamp(scrPos.x, marginx, ScrW() - marginx), math.Clamp(scrPos.y, marginy, ScrH() - marginy)
         surface.SetDrawColor(colorToUse.r, colorToUse.g, colorToUse.b, colorToUse.a)
         surface.DrawRect(x - size / 2, y - size / 2, size, size)
