d5e62a5b2e4dd20c86f1dddc1863e421eb782f75
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-24 00:58:15 +0100

    Store banned as char var

--- gamemode/core/derma/mainmenu/character.lua
+++ gamemode/core/derma/mainmenu/character.lua
@@ -487,7 +487,7 @@ function PANEL:createSelectedCharacterInfoPanel(character)
     local selectText = L("selectCharacter")
     if clientChar and character:getID() == clientChar:getID() then
         selectText = L("alreadyUsingCharacter")
-    elseif character:getData("banned") then
+    elseif character:getBanned() then
         selectText = L("bannedCharacter")
     end
 
@@ -495,7 +495,7 @@ function PANEL:createSelectedCharacterInfoPanel(character)
     self.selectBtn:SetSize(bw, bh)
     self.selectBtn:SetPos(cx, fy + fh + pad)
     self.selectBtn:SetText(selectText)
-    if clientChar and character:getID() == clientChar:getID() or character:getData("banned") then
+    if clientChar and character:getID() == clientChar:getID() or character:getBanned() then
         self.selectBtn:SetEnabled(false)
         self.selectBtn:SetTextColor(Color(255, 255, 255))
     end
