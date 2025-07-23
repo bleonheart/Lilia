5d161f5a625b0481bfcf061839a37315b46359ea
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-23 22:18:28 +0100

    Update player.lua

--- gamemode/core/meta/player.lua
+++ gamemode/core/meta/player.lua
@@ -20,8 +20,7 @@ end
 function playerMeta:hasPrivilege(privilegeName)
     local group = self:GetUserGroup()
     local perms = lia.admin.groups[group]
-    PrintTable(lia.admin.groups, 1)
-   // PrintTable(perms)
+    PrintTable(perms)
     if not perms then return false end
     return perms[privilegeName] == true
 end
