81c0847593609314573e18f009a7ce4f1d54653f
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-25 00:08:46 +0100

    Update client.lua

--- gamemode/core/hooks/client.lua
+++ gamemode/core/hooks/client.lua
@@ -285,6 +285,29 @@ function GM:OnChatReceived()
     if system.IsWindows() and not system.HasFocus() then system.FlashWindow() end
 end
 
+function GM:Move(client, moveData)
+    local char = client:getChar()
+    if char and client:GetMoveType() == MOVETYPE_WALK and moveData:KeyDown(IN_WALK) then
+        local mf, ms = 0, 0
+        local speed = client:GetWalkSpeed()
+        local ratio = lia.config.get("WalkRatio")
+        if moveData:KeyDown(IN_FORWARD) then
+            mf = ratio
+        elseif moveData:KeyDown(IN_BACK) then
+            mf = -ratio
+        end
+
+        if moveData:KeyDown(IN_MOVELEFT) then
+            ms = -ratio
+        elseif moveData:KeyDown(IN_MOVERIGHT) then
+            ms = ratio
+        end
+
+        moveData:SetForwardSpeed(mf * speed)
+        moveData:SetSideSpeed(ms * speed)
+    end
+end
+
 function GM:CreateMove(cmd)
     local client = LocalPlayer()
     if IsValid(client) and client:getLocalVar("bIsHoldingObject", false) and cmd:KeyDown(IN_ATTACK2) then
