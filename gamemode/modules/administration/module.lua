3ff3dd06c73421e4304ebad4c8dfa37970326fc0
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-25 06:57:16 +0100

    Update module.lua

--- gamemode/modules/administration/module.lua
+++ gamemode/modules/administration/module.lua
@@ -2,3 +2,8 @@
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.desc = "Provides a suite of administrative commands, configuration menus, and moderation utilities so staff can effectively manage the server."
+MODULE.Dependencies = {
+    {
+        Folder = "tools"
+    }
+}
