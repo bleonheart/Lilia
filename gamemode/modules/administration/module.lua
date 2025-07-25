750ebe23bcdbfe845be19f0570eb383a0da43f22
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-25 06:38:00 +0100

    Revert "Merge branch 'main' of https://github.com/bleonheart/Lilia"
    This reverts commit 4fd9fe307d573ad9b55a7b46d3cf942f8c1955ec, reversing
    changes made to b1caa9383e532cb610fde2650084a5e7a7e296be.

--- gamemode/modules/administration/module.lua
+++ gamemode/modules/administration/module.lua
@@ -1,4 +1,4 @@
-MODULE.name = "Administration Utilities"
+﻿MODULE.name = "Administration Utilities"
 MODULE.author = "Samael"
 MODULE.discord = "@liliaplayer"
 MODULE.desc = "Provides a suite of administrative commands, configuration menus, and moderation utilities so staff can effectively manage the server."
