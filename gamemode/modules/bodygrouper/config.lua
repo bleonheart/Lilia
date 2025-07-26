346afd90584319d1d506cc9f482f3394736deacd
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-26 22:49:46 +0100

    xd

--- gamemode/modules/bodygrouper/config.lua
+++
@@ -1,5 +0,0 @@
-﻿lia.config.add("BodyGrouperModel", "Body Grouper Model", "models/props_c17/FurnitureDresser001a.mdl", nil, {
-    desc = "Sets the model for the body grouper.",
-    category = "Gameplay",
-    type = "Generic"
-})
