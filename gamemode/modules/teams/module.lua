76f7a20410a2230913cf75ea86027cd52995a40e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-26 18:21:13 +0100

    Delete module.lua

--- gamemode/modules/teams/module.lua
+++
@@ -1,4 +0,0 @@
-﻿MODULE.name = "Teams"
-MODULE.author = "Samael"
-MODULE.discord = "@liliaplayer"
-MODULE.desc = "Handles the creation of factions and character classes, controlling default permissions and spawn settings for each."
