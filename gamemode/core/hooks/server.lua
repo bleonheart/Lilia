56cc226f3b4ea97c144d24bc41989db2b2047c0e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-26 18:49:24 +0100

    Update server.lua

--- gamemode/core/hooks/server.lua
+++ gamemode/core/hooks/server.lua
@@ -13,6 +13,50 @@ local logTypeMap = {
     looc = "chatLOOC"
 }
 
+function GM:CanPlayerUseChar(client, character)
+    if GetGlobalBool("characterSwapLock", false) and not client:hasPrivilege("Can Bypass Character Lock") then return false, L("serverEventCharLock") end
+    local factionData = lia.faction.indices[character:getFaction()]
+    if factionData and hook.Run("CheckFactionLimitReached", factionData, character, client) then return false, L("limitFaction") end
+    local bannedUntil = character:getBanned()
+    if isnumber(bannedUntil) and bannedUntil > os.time() then return false, L("bannedCharacter") end
+    return true
+end
+
+function GM:CanPlayerSwitchChar(client, character, newCharacter)
+    if character:getID() == newCharacter:getID() then return false, L("alreadyUsingCharacter") end
+    local banned = character:getBanned()
+    if banned and isnumber(banned) and banned > os.time() then return false, L("bannedCharacter") end
+    if not client:Alive() then return false, L("youAreDead") end
+    if client:hasRagdoll() then return false, L("youAreRagdolled") end
+    if client:hasValidVehicle() then return false, L("cannotSwitchInVehicle") end
+    if not client:isStaffOnDuty() then
+        local damageCooldown = lia.config.get("OnDamageCharacterSwitchCooldownTimer", 15)
+        if damageCooldown > 0 and client.LastDamaged and client.LastDamaged > CurTime() - damageCooldown then
+            lia.log.add(client, "permissionDenied", "switch character (recent damage)")
+            return false, L("tookDamageSwitchCooldown")
+        end
+
+        local switchCooldown = lia.config.get("CharacterSwitchCooldownTimer", 5)
+        local loginTime = character:getData("loginTime", 0)
+        if switchCooldown > 0 and loginTime + switchCooldown > os.time() then
+            lia.log.add(client, "permissionDenied", "switch character (cooldown)")
+            return false, L("switchCooldown")
+        end
+    end
+
+    local fac = lia.faction.indices[newCharacter:getFaction()]
+    if fac.OnCheckLimitReached then
+        if fac:OnCheckLimitReached(newCharacter, client) then return false, L("limitFaction") end
+    else
+        local lim = fac.limit
+        if isnumber(lim) then
+            local maxPlayers = lim < 1 and math.Round(player.GetCount() * lim) or lim
+            if team.NumPlayers(fac.index) >= maxPlayers then return false, L("limitFaction") end
+        end
+    end
+    return true
+end
+
 function GM:CharPreSave(character)
     local client = character:getPlayer()
     if not character:getInv() then return end
