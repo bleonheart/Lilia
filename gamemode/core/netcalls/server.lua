9e4e44b52833bfb8a51fedcdc797079a3d119fee
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-26 18:24:52 +0100

    xd

--- gamemode/core/netcalls/server.lua
+++ gamemode/core/netcalls/server.lua
@@ -430,7 +430,6 @@ net.Receive("lia_managesitrooms_action", function(_, client)
     end
 end)
 
-
 net.Receive("TransferMoneyFromP2P", function(_, sender)
     local amount = net.ReadUInt(32)
     local target = net.ReadEntity()
@@ -455,3 +454,22 @@ net.Receive("TransferMoneyFromP2P", function(_, sender)
     target:notifyLocalized("moneyTransferReceived", lia.currency.get(amount), senderName)
 end)
 
+net.Receive("msg", function(_, client)
+    local text = net.ReadString()
+    local charlimit = lia.config.get("MaxChatLength")
+    if charlimit > 0 then
+        if (client.liaNextChat or 0) < CurTime() and text:find("%S") then
+            hook.Run("PlayerSay", client, text)
+            client.liaNextChat = CurTime() + math.max(#text / 250, 0.4)
+        end
+    else
+        if utf8.len(text) > charlimit then
+            client:notifyLocalized("messageTooLong", charlimit)
+        else
+            if (client.liaNextChat or 0) < CurTime() and text:find("%S") then
+                hook.Run("PlayerSay", client, text)
+                client.liaNextChat = CurTime() + math.max(#text / 250, 0.4)
+            end
+        end
+    end
+end)
