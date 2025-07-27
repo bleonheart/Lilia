b127d73a95063d44fdaabf8a3c0c14a2b8b35e58
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-27 01:22:21 +0100

    Update staff.lua

--- gamemode/modules/administration/submodules/permissions/factions/staff.lua
+++ gamemode/modules/administration/submodules/permissions/factions/staff.lua
@@ -4,4 +4,4 @@ FACTION.color = Color(255, 56, 252)
 FACTION.isDefault = false
 FACTION.models = {"models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl"}
 FACTION.weapons = {"weapon_physgun", "gmod_tool"}
-FactionStaff = FACTION.index
+FACTION_STAFF = FACTION.index
