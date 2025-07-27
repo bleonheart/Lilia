105f7c68fc4559123b76a4e6a100af90cbb01d22
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-27 04:44:28 +0100

    Update server.lua

--- gamemode/modules/administration/submodules/staffmanagement/libraries/server.lua
+++ gamemode/modules/administration/submodules/staffmanagement/libraries/server.lua
@@ -1,5 +1,4 @@
 local MODULE = MODULE
-
 local function queryStaffActions(callback)
     lia.db.query([[SELECT admin, adminSteamID, action, COUNT(*) AS count FROM lia_staffactions GROUP BY adminSteamID, action]], callback)
 end
@@ -25,10 +24,9 @@ end)
 
 net.Receive("RequestTicketClaims", function(_, client)
     if not client:hasPrivilege("View Claims") then return end
-    lia.db.select({"timestamp", "requester", "requesterSteamID", "admin", "adminSteamID", "message"}, "ticketclaims")
-        :next(function(res)
-            net.Start("TicketClaims")
-            net.WriteTable(res.results or {})
-            net.Send(client)
-        end)
+    lia.db.select({"timestamp", "requester", "requesterSteamID", "admin", "adminSteamID", "message"}, "ticketclaims"):next(function(res)
+        net.Start("TicketClaims")
+        net.WriteTable(res.results or {})
+        net.Send(client)
+    end)
 end)
