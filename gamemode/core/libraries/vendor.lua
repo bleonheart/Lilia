5d64e6cbe51d1f679862f7fda556c3b1900539cf
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-28 01:52:23 +0100

    Allow editing vendor prices when preset active

--- gamemode/core/libraries/vendor.lua
+++ gamemode/core/libraries/vendor.lua
@@ -16,7 +16,7 @@ if SERVER then
     end
 
     lia.vendor.editor.price = function(vendor)
-        if vendor:getNetVar("preset") ~= "none" then return end
+        -- allow editing prices regardless of preset
         local itemType = net.ReadString()
         local price = net.ReadInt(32)
         vendor:setItemPrice(itemType, price)
