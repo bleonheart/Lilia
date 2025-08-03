e9d110b9d49be3f540bcb0fc773726b0faa05adf
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-04 00:19:19 +0100

    xd

--- gamemode/modules/teams/config.lua
+++ gamemode/modules/teams/config.lua
@@ -1,5 +1,1 @@
-﻿lia.config.add("ClassDisplay", "Display Classes on Characters", true, nil, {
-    desc = "Whether or not classes are displayed on characters",
-    category = "Character",
-    type = "Boolean"
-})
+﻿
