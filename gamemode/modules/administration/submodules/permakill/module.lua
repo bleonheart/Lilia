21ab0a98bd26fd8d060b03fb766e625f0fda7fa2
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-03 04:22:08 +0100

    Tie permakill to banned char var

--- gamemode/modules/administration/submodules/permakill/module.lua
+++ gamemode/modules/administration/submodules/permakill/module.lua
@@ -56,6 +56,15 @@ if SERVER then
             end
         end
     end)
+
+    function MODULE:CanPlayerUseChar(client, character)
+        if character:getData("banned") then
+            net.Start("PK_Notice")
+            net.WriteString(character:getName())
+            net.Send(client)
+            return false, L("bannedCharacter")
+        end
+    end
 else
     net.Receive("OpenPKViewer", function()
         local cases = net.ReadTable()
