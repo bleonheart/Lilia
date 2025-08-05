3d6e4bff0b3182186343ce603677a970be306596
Samael <33399712+bleonheart@users.noreply.github.com>
2025-08-05 04:29:19 +0100

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,14 +3,3 @@ GM.version = 7.732
 GM.Author = "Samael"
 GM.Website = "https://discord.gg/esCRH5ckbQ"
 include("core/libraries/loader.lua")
-local originalStart = net.Start
-local originalReceive = net.Receive
-function net.Start(name, unreliable)
-    if SERVER and util.NetworkStringToID(name) == 0 then util.AddNetworkString(name) end
-    return originalStart(name, unreliable)
-end
-
-function net.Receive(name, callback)
-    if SERVER and util.NetworkStringToID(name) == 0 then util.AddNetworkString(name) end
-    return originalReceive(name, callback)
-end
