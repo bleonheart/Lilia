974f105bccf5bc3458b3cc695c7c2b16821cbd0c
Samael <33399712+bleonheart@users.noreply.github.com>
2025-09-20 22:47:11 +0100

    Update notice.lua

--- gamemode/core/derma/panels/notice.lua
+++ gamemode/core/derma/panels/notice.lua
@@ -119,32 +119,6 @@ function PANEL:Paint(w, h)
 end
 
 vgui.Register("liaNotice", PANEL, "DPanel")
-local TimeFraction, CurTime = math.TimeFraction, CurTime
-local surfaceSetDrawColor, surfaceDrawRect, surfaceDrawOutlinedRect = surface.SetDrawColor, surface.DrawRect, surface.DrawOutlinedRect
-local PANEL = {}
-function PANEL:Init()
-    self:SetSize(256, 36)
-    self:SetContentAlignment(5)
-    self:SetExpensiveShadow(1, Color(0, 0, 0, 150))
-    self:SetFont("liaNoticeFont")
-    self:SetTextColor(color_white)
-    self:SetDrawOnTop(true)
-end
-
-function PANEL:Paint(w, h)
-    lia.util.drawBlur(self, 3, 2)
-    derma.SkinHook("Paint", "Panel", self, w, h)
-    if self.start then
-        local w2 = TimeFraction(self.start, self.endTime, CurTime()) * w
-        surfaceSetDrawColor(lia.config.get("Color"))
-        surfaceDrawRect(w2, 0, w - w2, h)
-    end
-
-    surfaceSetDrawColor(lia.config.get("Color"))
-    surfaceDrawOutlinedRect(0, 0, w, h)
-end
-
-vgui.Register("liaNotice", PANEL, "DLabel")
 PANEL = {}
 function PANEL:Init()
     self.padding = 80
