ee5093031d2484f5cde5818eedd1acf399b6f638
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-08 23:55:52 +0100

    xd

--- gamemode/modules/administration/itemspawner/server.lua
+++ gamemode/modules/administration/itemspawner/server.lua
@@ -1,31 +1,31 @@
-﻿local function fixupProp(client, ent, mins, maxs)
-    local pos = ent:GetPos()
-    local down, up = ent:LocalToWorld(mins), ent:LocalToWorld(maxs)
-    local trD = util.TraceLine({
-        start = pos,
-        endpos = down,
-        filter = {ent, client}
-    })
+﻿net.Receive("liaSpawnMenuSpawnItem", function(_, client)
+    local function fixupProp(client, ent, mins, maxs)
+        local pos = ent:GetPos()
+        local down, up = ent:LocalToWorld(mins), ent:LocalToWorld(maxs)
+        local trD = util.TraceLine({
+            start = pos,
+            endpos = down,
+            filter = {ent, client}
+        })
 
-    local trU = util.TraceLine({
-        start = pos,
-        endpos = up,
-        filter = {ent, client}
-    })
+        local trU = util.TraceLine({
+            start = pos,
+            endpos = up,
+            filter = {ent, client}
+        })
 
-    if trD.Hit and trU.Hit then return end
-    if trD.Hit then ent:SetPos(pos + trD.HitPos - down) end
-    if trU.Hit then ent:SetPos(pos + trU.HitPos - up) end
-end
+        if trD.Hit and trU.Hit then return end
+        if trD.Hit then ent:SetPos(pos + trD.HitPos - down) end
+        if trU.Hit then ent:SetPos(pos + trU.HitPos - up) end
+    end
 
-local function tryFixPropPosition(client, ent)
-    local m, M = ent:OBBMins(), ent:OBBMaxs()
-    fixupProp(client, ent, Vector(m.x, 0, 0), Vector(M.x, 0, 0))
-    fixupProp(client, ent, Vector(0, m.y, 0), Vector(0, M.y, 0))
-    fixupProp(client, ent, Vector(0, 0, m.z), Vector(0, 0, M.z))
-end
+    local function tryFixPropPosition(client, ent)
+        local m, M = ent:OBBMins(), ent:OBBMaxs()
+        fixupProp(client, ent, Vector(m.x, 0, 0), Vector(M.x, 0, 0))
+        fixupProp(client, ent, Vector(0, m.y, 0), Vector(0, M.y, 0))
+        fixupProp(client, ent, Vector(0, 0, m.z), Vector(0, 0, M.z))
+    end
 
-net.Receive("liaSpawnMenuSpawnItem", function(_, client)
     local id = net.ReadString()
     if not IsValid(client) or not id or not client:hasPrivilege("canUseItemSpawner") then return end
     local startPos, dir = client:EyePos(), client:GetAimVector()
