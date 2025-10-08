f3553b38f402ced3053db10a82608a778069ac4c
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-08 15:03:04 +0100

    XD
    X

--- gamemode/entities/weapons/distance_measure/shared.lua
+++ gamemode/entities/weapons/distance_measure/shared.lua
@@ -1,4 +1,4 @@
-﻿SWEP.PrintName = L("distanceMeasureTool")
+SWEP.PrintName = L("distanceMeasureTool")
 SWEP.Author = "Samael"
 SWEP.Contact = "@liliaplayer"
 SWEP.Purpose = L("measureDistanceBetweenTwoPoints")
@@ -14,6 +14,7 @@ SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 SWEP.Secondary.Ammo = "none"
 SWEP.StartPos = nil
+
 function SWEP:Initialize()
     self:SetHoldType("normal")
 end
@@ -24,17 +25,8 @@ function SWEP:PrimaryAttack()
     local tr = owner:GetEyeTrace()
     if not self.StartPos then
         self.StartPos = tr.HitPos
-        if CLIENT then
-            surface.PlaySound("buttons/button17.wav")
-            owner:ChatPrint(L("distanceMeasureStartPoint"))
-        end
     else
         local distance = self.StartPos:Distance(tr.HitPos)
-        if CLIENT then
-            surface.PlaySound("buttons/button17.wav")
-            owner:ChatPrint(L("distanceMeasureDistance", math.Round(distance)))
-        end
-
         self.StartPos = nil
     end
 end
@@ -42,19 +34,4 @@ end
 function SWEP:SecondaryAttack()
     if not IsFirstTimePredicted() then return end
     self.StartPos = nil
-    if CLIENT then
-        surface.PlaySound("buttons/button16.wav")
-        self:GetOwner():ChatPrint(L("distanceMeasureCancelled"))
-    end
-end
-
-function SWEP:DrawHUD()
-    if not self.StartPos then return end
-    local owner = self:GetOwner()
-    local tr = owner:GetEyeTrace()
-    local start = self.StartPos:ToScreen()
-    local endpos = tr.HitPos:ToScreen()
-    surface.SetDrawColor(255, 255, 0, 255)
-    surface.DrawLine(start.x, start.y, endpos.x, endpos.y)
-    surface.DrawCircle(start.x, start.y, 5, 255, 255, 0, 255)
 end
