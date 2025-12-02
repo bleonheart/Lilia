5f306c82f05a71662fd5f18cb13bc2e64db89f7b
Samael <33399712+bleonheart@users.noreply.github.com>
2025-12-02 20:51:37 +0000

    Update client.lua

--- gamemode/modules/doors/libraries/client.lua
+++ gamemode/modules/doors/libraries/client.lua
@@ -1,4 +1,4 @@
-﻿function MODULE:GetDoorInfo(entity, doorData, doorInfo)
+function MODULE:GetDoorInfo(entity, doorData, doorInfo)
     local owner = entity:GetDTEntity(0)
     local classes = doorData.classes or {}
     local factions = doorData.factions or {}
@@ -21,7 +21,7 @@
 
     if ownable and not IsValid(owner) then
         table.insert(doorInfo, {
-            text = L("doorCanBeOwned")
+            text = L("doorIsOwnable")
         })
     end
 
